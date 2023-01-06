ff062ae2f1f25ee3e8706c2a9c82c60c686f0aaa
SamY <91989013+Sammy949@users.noreply.github.com>
2023-01-06 23:45:40 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+from tkinter import Tk, messagebox, simpledialog
+
+def is_even(number):
+    return number % 2 == 0
+
+def get_even_letters(message):
+    even_letters = []
+    for counter in range(0, len(message)):
+        if is_even(counter):
+            even_letters.append(message[counter])
+    return even_letters
+
+def get_odd_letters(message):
+    odd_letters = []
+    for counter in range(0, len(message)):
+        if not is_even(counter):
+            odd_letters.append(message[counter])
+    return odd_letters
+
+def swap_letters(message):
+    letter_list = []
+    if not is_even(len(message)):
+        message = message + '#'
+    even_letters = get_even_letters(message)
+    odd_letters = get_odd_letters(message)
+
+    for counter in range(0, int(len(message)/2)):
+        letter_list.append(odd_letters[counter])
+        letter_list.append(even_letters[counter])
+    new_message = ''.join(letter_list)
+    return new_message
+
+def get_task():
+    task = simpledialog.askstring('Task', 'Do you want to encrypt or decrypt')
+    return task
+
+def get_message():
+    message = simpledialog.askstring('Message', 'Enter the secret message: ')
+    return message
+
+root = Tk()
+
+while True:
+    task = get_task()
+
+    if task == 'encrypt' or task == 'Encrypt':
+        message = get_message()
+        encrypted = swap_letters(message)
+        messagebox.showinfo('Message in encrypted version is:', encrypted)
+
+    elif task == 'decrypt' or task == 'Decrypt':
+        message = get_message()
+        decrypted = swap_letters(message)
+        messagebox.showinfo('Message in decrypted version:', decrypted)
+
+    else:
+        break
+
+root.mainloop()
