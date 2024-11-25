7e8309368ed22e3fb06f637160f617aa0c77f653
Sammy949 <urahsamuel0202@gmail.com>
2024-11-25 21:39:17 +0100

    Just a little cleanup

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from tkinter import Tk, messagebox, simpledialog
 import pyperclip
 
+
 def is_even(number):
     return number % 2 == 0
+
 
 def get_even_letters(message):
     even_letters = []
@@ -11,6 +13,7 @@ def get_even_letters(message):
             even_letters.append(message[counter])
     return even_letters
 
+
 def get_odd_letters(message):
     odd_letters = []
     for counter in range(0, len(message)):
@@ -18,32 +21,37 @@ def get_odd_letters(message):
             odd_letters.append(message[counter])
     return odd_letters
 
+
 def swap_letters(message):
     letter_list = []
     if not is_even(len(message)):
-        message = message + '#'
+        message = message + "#"
     even_letters = get_even_letters(message)
     odd_letters = get_odd_letters(message)
 
-    for counter in range(0, int(len(message)/2)):
+    for counter in range(0, int(len(message) / 2)):
         letter_list.append(odd_letters[counter])
         letter_list.append(even_letters[counter])
-    new_message = ''.join(letter_list)
+    new_message = "".join(letter_list)
     return new_message
+
 
 def remove_extra_character(message):
     # Remove the '#' if it exists at the end of the message
-    if message.endswith('#'):
+    if message.endswith("#"):
         message = message[:-1]
     return message
 
+
 def get_task():
-    task = simpledialog.askstring('Task', 'Do you want to encrypt or decrypt')
+    task = simpledialog.askstring("Task", "Do you want to encrypt or decrypt")
     return task
 
+
 def get_message():
-    message = simpledialog.askstring('Message', 'Enter the secret message: ')
+    message = simpledialog.askstring("Message", "Enter the secret message: ")
     return message
+
 
 root = Tk()
 root.withdraw()  # Hide the root window
@@ -51,17 +59,24 @@ root.withdraw()  # Hide the root window
 while True:
     task = get_task()
 
-    if task in ('encrypt', 'Encrypt'):
+    if task in ("encrypt", "Encrypt"):
         message = get_message()
         encrypted = swap_letters(message)
         pyperclip.copy(encrypted)  # Copy to clipboard
-        messagebox.showinfo('Encrypted Message', f'Message in encrypted version is: {encrypted}\n\nThe message has been copied to the clipboard!')
+        messagebox.showinfo(
+            "Encrypted Message",
+            f"Message in encrypted version is: {encrypted}\n\nThe message has been copied to the clipboard!",
+        )
 
-    elif task in ('decrypt', 'Decrypt'):
+    elif task in ("decrypt", "Decrypt"):
         message = get_message()
         decrypted = swap_letters(message)
-        decrypted = remove_extra_character(decrypted)  # Remove the extra character if present
-        messagebox.showinfo('Decrypted Message', f'Message in decrypted version: {decrypted}')
+        decrypted = remove_extra_character(
+            decrypted
+        )  # Remove the extra character if present
+        messagebox.showinfo(
+            "Decrypted Message", f"Message in decrypted version: {decrypted}"
+        )
 
     else:
         break
